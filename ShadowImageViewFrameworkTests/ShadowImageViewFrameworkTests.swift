67c94fdb73dbd8b854a2f0c5a6773ca3d9aa9854
olddonkey <olddonkeyblog@gmail.com>
2017-05-07 11:23:00 -0500

    Add Carthage support

---
+++ ShadowImageViewFrameworkTests/ShadowImageViewFrameworkTests.swift
@@ -0,0 +1,36 @@
+//
+//  ShadowImageViewFrameworkTests.swift
+//  ShadowImageViewFrameworkTests
+//
+//  Created by olddonkey on 2017/5/7.
+//  Copyright © 2017年 olddonkey. All rights reserved.
+//
+
+import XCTest
+@testable import ShadowImageViewFramework
+
+class ShadowImageViewFrameworkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
